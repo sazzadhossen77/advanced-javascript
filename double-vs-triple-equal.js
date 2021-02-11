a86206ad692c18d58ab2d3fb75b85d70f0e75e9e
sazzadhossen77 <sazzadhosseb379@gmail.com>
2021-02-12 01:13:33 +0600

    double vs triple equal js

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,20 @@
+  
+const first = 12;
+const second = "12";
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+  
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
