77d27e3f32e2761b82211cdc3373bcbb35a3b6e7
sazzadhossen77 <sazzadhosseb379@gmail.com>
2021-02-12 02:20:54 +0600

    object property

---
+++ objects.js
@@ -0,0 +1,14 @@
+const students = [
+    {id: 21, name: 'Akib Hasan'},
+    {id: 31, name: 'Mahim'},
+    {id: 41, name: 'Piplo'},
+    {id: 71, name: 'Shuvo'}
+];
+
+const names = students.map( s => s.name);
+const ids = students.map(s => s.id);
+const bigger = students.filter( s => s.id>40);
+const biggerOne= students.find( s => s.id>40);
+
+
+console.log(biggerOne);
