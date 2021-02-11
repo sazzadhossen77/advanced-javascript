b12f899735ab4b5228eb94335e74e482ee5595d8
sazzadhossen77 <sazzadhosseb379@gmail.com>
2021-02-12 00:46:05 +0600

    null vs undefined js

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+function add(num1, num2) {
+    total = num1 + num2;
+    console.log(total);
+    return
+}
+
+function add(num1, num2) {
+         console.log(num1,num2);
+    }
+
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9, 10]
+console.log(ages[12]);
