90f70d5544426252431a601321f059f14688747a
sazzadhossen77 <sazzadhosseb379@gmail.com>
2021-02-13 01:27:44 +0600

    es6

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [2, 3, 4, 5, 7, 8, 9];
+const part = nums.slice(2, 5);
+//console.log(part);
+const removed = nums.splice(2,3, 77);
+// console.log(removed);
+// console.log(nums);
+
+const together = nums.join("ami")
+console.log(together);
--- objects.js
+++ objects.js
@@ -7,8 +7,8 @@ const students = [
 
 const names = students.map( s => s.name);
 const ids = students.map(s => s.id);
-const bigger = students.filter( s => s.id>40);
- const biggerOne= students.find( s => s.id>40);
+//const bigger = students.filter( s => s.id>40);
+const biggerOne= students.find( s => s.id>40);
 
 console.log(biggerOne);
 console.log(bigger);
